63419747be3ba3b0ef03a47b70d25384a0ce33d9
Scott Gale <galejscott@gmail.com>
2019-05-09 11:07:33 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -87,7 +87,7 @@ def game_hash
       :slam_dunks => 2
       },
       {
-      :name => "Bismak Biyombo", 
+      :name => "Bismack Biyombo", 
       :number => 0,
       :shoe => 16,
       :points => 12,
